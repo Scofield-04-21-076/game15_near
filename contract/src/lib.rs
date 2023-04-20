e2ede93ad3e1fec28713e9f1dcd41d3dcc1c01a8
vitalii <vitaliy.kazmirevskiy@elephantslab.com>
2023-04-20 10:35:27 +0300

    skelet

---
+++ contract/src/lib.rs
@@ -0,0 +1,13 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Pazzle {
+
+}
+
+#[near_bindgen]
+impl Pazzle {
+
+}
