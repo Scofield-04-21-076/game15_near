52b0dee8df04240f0a99db80f66ee0778f9cefd9
vitalii <vitaliy.kazmirevskiy@elephantslab.com>
2023-04-28 15:42:14 +0300

    to array

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,10 +1,10 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{AccountId, BorshStorageKey, env, log, near_bindgen, require};
-use near_sdk::collections::{LookupMap, Vector};
+use near_sdk::collections::{LookupMap};
 use near_sdk::serde::{Deserialize, Serialize};
 
 
-const SIZE: u8 = 16;
+const SIZE: usize = 16;
 
 #[derive(BorshSerialize, BorshStorageKey)]
 pub enum StorageKey{
@@ -32,7 +32,7 @@ impl Default for Player {
 #[derive(Default, BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
 #[serde(crate = "near_sdk::serde" )]
 pub struct Game {
-    pub tiles: String,
+    pub tiles: [u8; SIZE],
 
 }
 
@@ -56,7 +56,7 @@ impl Default for Pazzle {
 
 #[near_bindgen]
 impl Pazzle {
-    pub fn new_game(&mut self, shuffle: String) {
+    pub fn new_game(&mut self, shuffle: [u8; SIZE]) {
 
         self.check_tiles(shuffle.clone());
 
@@ -73,14 +73,12 @@ impl Pazzle {
     }
 
     #[private]
-    pub fn is_solvable(&self, tiles: String) -> bool {
-        let mut count_inversions:u8 = 0;
-
-        let tiles_vec = self.string_to_vector(tiles);
+    pub fn is_solvable(&self, tiles: [u8; SIZE]) -> bool {
+        let mut count_inversions: u8 = 0;
 
         for i in 0..SIZE-1 {
             for j in 0..i {
-                if tiles_vec.get(j as u64) > tiles_vec.get(i as u64) {
+                if tiles[j] > tiles[i] {
                     count_inversions += 1;
                 }
             }
@@ -91,86 +89,40 @@ impl Pazzle {
 
 
 
-    pub fn run(&mut self, tiles: String) {
+    pub fn run(&mut self, tiles: [u8; SIZE]) {
 
-        let tiles_vec = self.string_to_vector(tiles.clone());
+        let mut tiles_copy = tiles.clone();
 
-        for i in 0..SIZE {
-
-            log!(
-            r#"EVENT_JSON:{{
-                "standard":"",
-                "version":"1.0.0",
-                "event":"1",
-                "data":[{{"tiles_vec":"{}",
-                    }}]}}"#,
-            self.expect_value_found(tiles_vec.get(i as u64)),
-        );
-        }
         self.check_tiles(tiles.clone());
 
         let x: u8;
         let x0: u8;
 
-        let mut x_x0: Vector<u8> = Vector::new(b"vec-uid-1".to_vec());
+        let mut x_x0: Vec<u8> = Vec::new();
 
         let mut game = self.expect_value_found(
             self.games.get(&env::predecessor_account_id()));
-        let game_tiles = self.string_to_vector(game.tiles.clone());
+        let game_tiles = game.tiles.clone();
 
         for i in 0..SIZE {
 
-            log!(
-            r#"EVENT_JSON:{{
-                "standard":"",
-                "version":"1.0.0",
-                "event":"2",
-                "data":[{{"game_tiles":"{:?}",
-                    }}]}}"#,
-            game_tiles.get(i as u64),
-        );
-        }
-        for i in 0..SIZE {
+            if game_tiles[i] != tiles[i] {
 
-            log!(
-            r#"EVENT_JSON:{{
-                "standard":"",
-                "version":"1.0.0",
-                "event":"3",
-                "data":[{{"tiles_vec":"{:?}",
-                    }}]}}"#,
-            tiles_vec.get(i as u64),
-        );
-
-            log!(
-            r#"EVENT_JSON:{{
-                "standard":"",
-                "version":"1.0.0",
-                "event":"4",
-                "data":[{{"game_tiles":"{:?}",
-                    }}]}}"#,
-           game_tiles.get(i as u64),
-        );
-
-            if self.expect_value_found(
-                game_tiles.get(i as u64)) !=
-                self.expect_value_found(tiles_vec.get(i as u64)) {
-
-                x_x0.push(&i);
+                x_x0.push(i as u8);
             }
         }
 
         require!(x_x0.len() != 0, "the move was not made");
         require!(x_x0.len() == 2, "only one permutation can be done in one turn");
 
-        match self.expect_value_found(tiles_vec.get(self.expect_value_found(x_x0.get(0)) as u64)) {
+        match tiles[x_x0[0] as usize] {
             0 => {
-                x = self.expect_value_found(x_x0.get(0));
-                x0 = self.expect_value_found(x_x0.get(1));
+                x = x_x0[0];
+                x0 = x_x0[1];
             },
             _ => {
-                x0 = self.expect_value_found(x_x0.get(0));
-                x = self.expect_value_found(x_x0.get(1));
+                x0 = x_x0[0];
+                x = x_x0[1];
             }
         }
 
@@ -178,39 +130,24 @@ impl Pazzle {
         let i_x0: i8 = x0 as i8;
 
         require!(
-            self.expect_value_found(game_tiles.get(x.into())) == 0 &&
+            game_tiles[x as usize] == 0 &&
             ((i_x % 4 != 0 && i_x0 % 4 != 3) ||
                 (i_x0 % 4 != 0 && i_x % 4 != 3)) &&
             (((i_x0 - i_x) == 1 || (i_x0 - i_x) == -1 ) ||
                 ((i_x0 - i_x) == 4 || (i_x0 - i_x) == -4 )),
             "not a correct move");
 
-        let value = self.expect_value_found(tiles_vec.get(x0 as u64));
+        let value = tiles_copy[x0 as usize];
+        tiles_copy[x as usize] = value;
+        tiles_copy[x0 as usize] = 0;
 
-        let mut vec_to_string: String = "".to_string();
-        for i in 0..SIZE {
-            vec_to_string +=
-                &self.expect_value_found(game_tiles.get(i as u64)).to_string();
-            if i != SIZE - 1 {
-                vec_to_string += ",";
-            }
-        }
-
-        vec_to_string = vec_to_string.replace(
-            &(self.expect_value_found(tiles_vec.get(x as u64))).to_string(),
-            &value.to_string());
-
-        vec_to_string = vec_to_string.replace(
-            &(self.expect_value_found(tiles_vec.get(x0 as u64))).to_string(),
-            "0");
-
-        game.tiles = vec_to_string;
+        game.tiles = tiles_copy;
 
         self.games.insert(&env::predecessor_account_id(), &game);
         log!("the move is successful");
     }
 
-    pub fn get_tiles(&self) -> String {
+    pub fn get_tiles(&self) -> [u8; SIZE] {
         let game = self.expect_value_found(
             self.games.get(&env::predecessor_account_id()));
 
@@ -226,34 +163,21 @@ impl Pazzle {
 }
 
 impl Pazzle {
-    pub fn string_to_vector(&self, tiles: String) -> Vector<u8>{
-        let mut vector: Vector<u8> = Vector::new(b"vec-uid-1".to_vec());
 
-        let v: Vec<u8> = tiles.split(',').map(|x| x.parse::<u8>().unwrap()).collect();
+    pub fn check_tiles(&self, tiles: [u8; SIZE]) {
 
-        for x in &v {
-            vector.push(x);
-        }
-
-        vector
-    }
-
-    pub fn check_tiles(&self, tiles: String) {
-
-        let tiles_vec = self.string_to_vector(tiles);
-
-        require!(tiles_vec.len() == SIZE as u64,
+        require!(tiles.len() == SIZE,
             "unexpected number of values (16 needed)");
 
         for i in 0..SIZE {
-            let buff = tiles_vec.get(i as u64);
+            let buff = tiles[i];
 
-            require!(buff >= Some(0) && buff <= Some(15),
+            require!(buff <= 15,
                 "unexpected number of values (0-15 needed)");
 
             for j in 0..SIZE {
                 if i != j {
-                    require!(buff != tiles_vec.get(j as u64),
+                    require!(buff != tiles[j],
                         "unexpected number of values (repetition of values)");
                 }
             }
@@ -265,15 +189,12 @@ impl Pazzle {
         let game = self.expect_value_found(
             self.games.get(&env::predecessor_account_id()));
 
-        if game.tiles.chars().last().unwrap() != '0' {
+        if game.tiles[SIZE-1] != 0 {
             return false;
         }
 
-        let vector = self.string_to_vector(game.tiles);
-
         for i in (0..SIZE).rev() {
-            if self.expect_value_found(
-                vector.get(i as u64)) != (i + 1) as u8 {
+            if game.tiles[i] != (i + 1) as u8 {
                 return false;
             }
         }
